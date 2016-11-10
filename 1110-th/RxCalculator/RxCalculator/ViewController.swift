40471c3babf5630e5416f52430a965440f452dd2
Brian Hans <wbrianw8@gmail.com>
2016-11-10 11:45:25 -0800

    Inital commit

--- 1110-th/RxCalculator/RxCalculator/ViewController.swift
+++ 1110-th/RxCalculator/RxCalculator/ViewController.swift
@@ -7,21 +7,46 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
-  @IBOutlet weak var operationSegmentedControl: UISegmentedControl!
-  @IBOutlet weak var firstValueTextField: UITextField!
-  @IBOutlet weak var secondValueTextField: UITextField!
-  @IBOutlet weak var operationLabel: UILabel!
-  @IBOutlet weak var resultLabel: UILabel!
-  
-
-  // MARK: View Controller Lifecycle
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-
+    
+    let disposeBag = DisposeBag()
+    
+    @IBOutlet weak var operationSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var firstValueTextField: UITextField!
+    @IBOutlet weak var secondValueTextField: UITextField!
+    @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    // MARK: View Controller Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let operationObservable: Observable<Calculator.Operation> = operationSegmentedControl.rx.value.map { selectedIndex in
+            let operation = Calculator.Operation(rawValue: selectedIndex) ?? Calculator.Operation.addition
+            return operation
+        }
+        
+        let firstValueObserver: Observable<Int> = firstValueTextField.rx.text.map{ string in
+            return Int(string ?? "") ?? 0
+        }
+        
+        let secondValueObserver: Observable<Int> = secondValueTextField.rx.text.map{ string in
+            return Int(string ?? "") ?? 0
+        }
+        
+        let resultObservable = Observable.combineLatest(firstValueObserver, secondValueObserver) {(firstValue, secondValue) in
+            return firstValue + secondValue
+        }
+        
+        resultObservable.map{(resultInt) in
+            return String(resultInt)
+        }.bindTo(resultLabel.rx.text).addDisposableTo(disposeBag)
+    }
+    
 }
 
